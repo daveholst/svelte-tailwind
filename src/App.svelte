87bb59dbaf425cefe69c838f333ee116c9e2081b
Dave Holst <daveholst@gmail.com>
2022-11-10 09:25:13 +0800

    finished for now + added surge deploy script

--- src/App.svelte
+++ src/App.svelte
@@ -137,5 +137,108 @@
   </div>
 </section>
 
+<!-- Call to Action -->
+<section id="cta" class="bg-brightRed">
+  <div
+    class=" container mx-auto flex flex-col items-center justify-between space-y-12 px-6 py-24 md:flex-row md:space-y-0 md:py-12"
+  >
+    <h2
+      class="text-center text-4xl font-bold leading-tight text-white md:max-w-xl md:text-left md:text-3xl"
+    >
+      Simplify how your team works today
+    </h2>
+    <a
+      href="/"
+      class="rounded-full bg-white px-5 py-2 text-brightRed shadow-2xl hover:bg-gray-600"
+    >
+      Get Started
+    </a>
+  </div>
+</section>
+
+<!-- Footer -->
+<footer>
+  <div class="bg-veryDarkBlue">
+    <div
+      class="container mx-auto flex flex-col-reverse justify-between space-y-8 px-6 py-10 lg:flex-row lg:space-y-0"
+    >
+      <!-- Logo & Social Links Container -->
+      <div
+        class="flex flex-col-reverse items-center justify-between space-y-12 lg:flex-col lg:items-start lg:space-y-0"
+      >
+        <div class="mx-auto my-6 text-center text-white lg:hidden">
+          Copyright &copy; 2022, All Rights Reserved
+        </div>
+        <!-- Logo -->
+        <div>
+          <img src="img/logo-white.svg" class="h-8" alt="" />
+        </div>
+        <!-- Social Links -->
+        <div class="flex justify-center space-x-4">
+          <a href="#">
+            <img src="img/icon-facebook.svg" class="h-8" alt="" />
+          </a>
+          <a href="#">
+            <img src="img/icon-youtube.svg" class="h-8" alt="" />
+          </a>
+          <a href="#">
+            <img src="img/icon-instagram.svg" class="h-8" alt="" />
+          </a>
+          <a href="#">
+            <img src="img/icon-pinterest.svg" class="h-8" alt="" />
+          </a>
+          <a href="#">
+            <img src="img/icon-twitter.svg" class="h-8" alt="" />
+          </a>
+        </div>
+      </div>
+      <!-- List Container -->
+      <div class="flex justify-around space-x-32">
+        <div class="flex flex-col space-y-3 text-white">
+          <a href="#" class="hover:tecxt-brightRed">Home</a>
+          <a href="#" class="hover:tecxt-brightRed">Pricing</a>
+          <a href="#" class="hover:tecxt-brightRed">Products</a>
+          <a href="#" class="hover:tecxt-brightRed">About</a>
+        </div>
+        <div class="flex flex-col space-y-3 text-white">
+          <a href="#" class="hover:tecxt-brightRed">Careers</a>
+          <a href="#" class="hover:tecxt-brightRed">Community</a>
+          <a href="#" class="hover:tecxt-brightRed">Privacy Policy</a>
+        </div>
+      </div>
+
+      <!-- Input Container -->
+      <div class="flex flex-col justify-between">
+        <form action="">
+          <div class="flex space-x-3">
+            <input
+              type="text"
+              class="flex-1 rounded-full px-4 focus:outline-none"
+              placeholder="Updated in your inbox"
+            />
+            <button
+              class="rounded-full bg-brightRed px-6 py-2 text-white hover:bg-brightRedLight focus:outline-none"
+              >Go</button
+            >
+          </div>
+        </form>
+        <div class="hidden text-white lg:block">
+          Copyright &copy; 2022, All Rights Reserved
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
 <style>
+  #cta {
+    background-image: url('img/bg-simplify-section-desktop.svg');
+    background-repeat: no-repeat;
+  }
+
+  @media (max-width: 576px) {
+    #cta {
+      background-image: url('img/bg-simplify-section-mobile.svg');
+    }
+  }
 </style>
--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -8,6 +8,8 @@
     'Careers',
     'Community'
   ];
+
+  let menuOpen = false;
 </script>
 
 <!-- NavBar -->
@@ -26,5 +28,80 @@
     </div>
     <!-- Button -->
     <Button class="hidden" label="Get Started" />
+    <!-- Hamburger Icon -->
+    <button
+      id="menu-btn"
+      class="block focus:outline-none md:hidden"
+      class:menu-open={menuOpen}
+      on:click={() => (menuOpen = !menuOpen)}
+    >
+      <span class="hamburger-top" />
+      <span class="hamburger-mid" />
+      <span class="hamburger-bot" />
+    </button>
+  </div>
+
+  <!-- Mobile Menu -->
+  <div class="md:hidden">
+    <div
+      id="menu"
+      class="absolute left-6 right-6 mt-10 hidden flex-col items-center space-y-6 self-end bg-white py-8 font-bold drop-shadow-md sm:w-auto sm:self-center"
+      class:show-menu={menuOpen}
+    >
+      <a href="#">Pricing</a>
+      <a href="#">Product</a>
+      <a href="#">About Us</a>
+      <a href="#">Careers</a>
+      <a href="#">Community</a>
+    </div>
   </div>
 </nav>
+
+<style>
+  #menu-btn {
+    cursor: pointer;
+    width: 24px;
+    height: 24px;
+    transition: all 0.25s;
+    position: relative;
+  }
+
+  .hamburger-top,
+  .hamburger-mid,
+  .hamburger-bot {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 24px;
+    height: 2px;
+    background: #000;
+    transform: rotate(0);
+    transition: all 0.5s;
+  }
+
+  .hamburger-mid {
+    transform: translateY(7px);
+  }
+  .hamburger-bot {
+    transform: translateY(14px);
+  }
+
+  .menu-open {
+    transform: rotate(90deg);
+    transform: translateY(0px);
+  }
+
+  .menu-open .hamburger-top {
+    transform: rotate(45deg) translateY(6px) translate(6px);
+  }
+  .menu-open .hamburger-mid {
+    display: none;
+  }
+  .menu-open .hamburger-bot {
+    transform: rotate(-45deg) translateY(6px) translate(-6px);
+  }
+
+  .show-menu {
+    display: flex;
+  }
+</style>
